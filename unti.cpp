09e762cd3a6460aa105cd5d828d82f1bfb51c8fd
Mostafa Saeed <mostafasaeed483@gmail.com>
2019-03-18 02:08:03 +0200

    Add files via upload

---
+++ unti.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long n,a,b;
+
+int main() 
+{
+	cin >> n ;
+	cin >> a>> b;
+	
+	long long ws = max(a,b)-1;
+	long long bs = n-min(a,b) ;
+	
+	cout << (ws>bs ? "Black":"White");
+		
+	
+	return 0;
+}
